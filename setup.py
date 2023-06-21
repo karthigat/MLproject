a39e4209b85c52aad1a2270b466a881bec70f1ea
karthiga <karthigathangavel.2011@gmail.com>
2023-06-20 21:18:21 -0700

    setup and requirements

---
+++ setup.py
@@ -0,0 +1,29 @@
+# setup.py can be used to install the created project as a package
+#building the application as a package
+#it is metadata information
+from setuptools import find_packages, setup
+from typing import List
+
+HYPEN_E_DOT = '-e .'
+def get_requirements(file_path:str)->List[str]:
+    '''
+        this function will return the list of requirements
+    '''
+    requirements = []
+    with open(file_path) as file_obj:
+        requirements = file_obj.readlines()
+        requirements = [req.replace("\n", "") for req in requirements]
+        
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
+    
+    return requirements
+
+setup(
+    name='ML_project',
+    version='0.0.1',
+    author='Karthiga',
+    author_email='karthigathangavel.2011@gmail.com',
+    packages=find_packages(),
+    install_requires = get_requirements('requirement.txt')
+)
